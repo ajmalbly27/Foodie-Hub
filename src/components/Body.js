041de016435982f24c3b9a0185711928d5c72351
MohdAjmalRaza <ajmalbly27@gmail.com>
2024-09-03 19:56:38 +0530

    feat - search functionality and Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,75 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  return (
+  //Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body rendered");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9352403&lng=77.624532&str=Biryani&trackingId=undefined&submitAction=ENTER&queryUniqueId=30b92518-9086-a42b-8938-90eb3f317fe0&selectedPLTab=RESTAURANT"
+    );
+
+    //use in case of trying to bypass cors policy without using cors chrome plugin
+    // const data = await fetch(
+    //   "https://corsproxy-la3g.onrender.com/full/?url=https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9352403&lng=77.624532&str=Biryani&trackingId=undefined&submitAction=ENTER&queryUniqueId=30b92518-9086-a42b-8938-90eb3f317fe0&selectedPLTab=RESTAURANT"
+    // );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurants(
+      json?.data?.cards[0]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[0]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.card.card.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard
             key={restaurant.card.card.info.id}
             resData={restaurant}
@@ -15,4 +79,5 @@ const Body = () => {
     </div>
   );
 };
+
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("Header render");
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
-        />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
         <ul>
@@ -13,6 +16,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
